52e96f6afd29c51d7f38e1d6014099d3c7ada665
Vu Le Nam <namvule@hotmail.com>
2018-10-15 11:03:58 +0900

    update product routes

--- shop/product/routes.py
+++ shop/product/routes.py
@@ -3,10 +3,18 @@ from flask_login import current_user, login_required
 
 from shop import app, db
 from shop.product import bp
+from shop.models import Photo, Cost, Product
 
 @bp.route('/', methods=['GET', 'POST'])
 @login_required
 def index():
+    if request.method is 'GET':
+        row_no = request.args.get('r') # number of row on one page
+        current_page = request.args.get('page')
+        products = Product.query.all
+    else:
+        # TODO:
+        print("method is POST. TODO ")
     return render_template('product/index.html')
 
 @bp.route('/new', methods=['GET', 'POST'])
