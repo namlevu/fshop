c715d8c8bdbee4c74a569faca002654a5b8059b3
Vu Le Nam <namvule@hotmail.com>
2018-10-15 11:03:15 +0900

    make setup file

---
+++ setup.py
@@ -0,0 +1,20 @@
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='bshop-flask',
+    version='1.0.0',
+    description='shop product management application',
+    long_description=long_description,
+    url='http://www.namvl.com',
+    license='Apache-2.0'
+)
