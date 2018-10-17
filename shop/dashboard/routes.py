b3c71214c94421cb53c541e6701c537a10f79c7f
namvule <namvule@hotmail.com>
2018-10-17 16:27:34 +0900

    add dashboad module

---
+++ shop/dashboard/routes.py
@@ -0,0 +1,15 @@
+# shop / dashboard / routes class
+
+# /d/product
+# get -> select product ( one or all )
+# post -> new product
+# put -> update product
+# delete -> delete product
+
+# /d/user
+# get/post/put/delete
+
+# /d/cost
+# /d/photo
+
+
