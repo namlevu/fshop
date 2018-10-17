f97d3ec272b76f6b4083da26ffde2dee2c3a5af9
Vu Le Nam <namvule@hotmail.com>
2018-10-17 18:22:22 +0900

    change config to file

--- config.py
+++ config.py
@@ -1,14 +1,26 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+from configparser import ConfigParser
 
 class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'bshop-app.db')
-
-    #CONNECTION_STRING = "mongodb://username:password@mshop-shard-00-00-wuufk.gcp.mongodb.net:27017,mshop-shard-00-01-wuufk.gcp.mongodb.net:27017,mshop-shard-00-02-wuufk.gcp.mongodb.net:27017/test?ssl=true&replicaSet=mshop-shard-0&authSource=admin&retryWrites=true"
-    MONGODB_DB = "test"
-    MONGODB_HOST = "mshop-shard-00-00-wuufk.gcp.mongodb.net,mshop-shard-00-01-wuufk.gcp.mongodb.net,mshop-shard-00-02-wuufk.gcp.mongodb.net"
-    MONGODB_PORT = 27017
-    MONGODB_USERNAME = "username"
-    MONGODB_PASSWORD = "password"
+    appconfig = ConfigParser()
+    appconfig.read('config.ini')
+    if 'PRODUCTION' in appconfig:
+        print('Production configuration load success')
+        MONGODB_DB = appconfig['PRODUCTION']['mongodb_db']
+        MONGODB_HOST = appconfig['PRODUCTION']['mongodb_host']
+        MONGODB_PORT = int(appconfig['PRODUCTION']['mongodb_port'])
+        MONGODB_USERNAME = appconfig['PRODUCTION']['mongodb_username']
+        MONGODB_PASSWORD = appconfig['PRODUCTION']['mongodb_password']
+    elif 'DEV' in appconfig:
+        print('Development configuration load success')
+        MONGODB_DB = appconfig['DEV']['mongodb_db']
+        MONGODB_HOST = appconfig['DEV']['mongodb_host']
+        MONGODB_PORT = int(appconfig['DEV']['mongodb_port'])
+        MONGODB_USERNAME = appconfig['DEV']['mongodb_username']
+        MONGODB_PASSWORD = appconfig['DEV']['mongodb_password']
+    else:
+        print('Load configuration error')
