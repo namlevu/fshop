e5356fdd202160f573d8bd5d2b6e25ebb2656894
namvule <namvule@hotmail.com>
2018-10-17 15:41:17 +0900

    update db

--- shop/__init__.py
+++ shop/__init__.py
@@ -3,18 +3,24 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+#from flask_pymongo import PyMongo
+#from flask.ext.mongoalchemy import MongoAlchemy
+from flask_mongoalchemy import MongoAlchemy
 
 app = Flask(__name__)
 
 app.secret_key = b'www.namvl.com@ln'
 app.config.from_object(Config)
-db = SQLAlchemy(app)
+
+#db = SQLAlchemy(app)
+db = MongoAlchemy(app)
 migrate = Migrate(app, db)
+#mongo = PyMongo(app)
 login = LoginManager(app)
 login.login_view = 'auth.login'
 
-global first_time
-first_time = True
+#global first_time
+#first_time = True
 # -------------------------------------------------
 from shop.auth import bp as auth_bp
 app.register_blueprint(auth_bp, url_prefix='/auth')
